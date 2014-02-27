3b415d02cc30648578f77cf4deff570753639461
isgiker <isgiker@gmail.com>
2014-02-27 08:12:00 +0800

    www reg

--- supermarket/front_www/controllers/Reg.php
+++ supermarket/front_www/controllers/Reg.php
@@ -21,6 +21,21 @@ class RegController extends Core_Controller_Www {
             $v = new validation(); //数据校验
             $v->parentCls=$this;
             $v->validate($rules, $post);
+            
+            //检验用户名和邮箱是否有人注册
+            $email_exists=$this->model->checkEmail($post['email']);
+            if($email_exists){
+                if(!isset($v->error_message['email']) || !$v->error_message['email']){
+                    $v->error_message['email']='该邮箱已有人注册！';
+                }
+            }
+            $uname_exists=$this->model->checkUserName($post['nickname']);
+            if($uname_exists){
+                if(!isset($v->error_message['nickname']) || !$v->error_message['nickname']){
+                    $v->error_message['nickname']='该昵称已有人注册！';
+                }
+            }
+            
             if (!empty($v->error_message)) {
                 $this->getView()->assign("error", $v->error_message); //输出同步错误信息
                 $this->getView()->assign("post", $post);
@@ -30,6 +45,7 @@ class RegController extends Core_Controller_Www {
             }else{
                 $this->saveAction($post, 'reg');
             }
+            
             if(isset($post['provinceId']) && $post['provinceId']){
                 $city = $this->areaModel->getNodeArea($post['provinceId']);
                 $this->getView()->assign("city", $city);
@@ -51,6 +67,42 @@ class RegController extends Core_Controller_Www {
         $this->getView()->assign("province", $province);
         $this->getView()->assign("rules", json_decode($rules)->validation);
         
+    }
+    
+    public function saveAction($data,$action){
+        if(!$data || !$action){
+            if($this->isAjax()){
+                $this->err(null, '参数错误');
+            }else{
+                $this->redirect('/'.$this->getRequest()->getModuleName().'/'.$this->getRequest()->getControllerName().'/'.$action);
+            }
+            
+        }
+        //保存数据 begin
+        $saveR = $this->model->$action($data);
+        
+        $_event_success_Url = "http://chaoshi.jiada.local";
+        $_event_fail_Url = "/$this->_ModuleName/$this->_ControllerName/reg";
+        if($saveR){
+            //如果是注册成功，则发送激活邮件
+            if($action=='reg'){
+                
+            }
+            
+            //保存成功跳转到列表页            
+            if($this->isAjax()){
+                $this->ok(null, $_event_success_Url, '保存成功！');
+            }else{
+                $this->getView()->assign("_event", array('_eventMsg'=>'保存成功！','_eventUrl'=>$_event_success_Url));
+            }
+        }else{
+            if($this->isAjax()){
+                $this->ok(null, $_event_fail_Url, '保存失败！');
+            }else{
+                $this->getView()->assign("_event", array('_eventMsg'=>'保存失败！','_eventUrl'=>$_event_fail_Url));
+            }
+            
+        }
     }
 
 
--- supermarket/front_www/models/Reg.php
+++ supermarket/front_www/models/Reg.php
@@ -6,99 +6,75 @@
  */
 class RegModel extends BasicModel{
 
-    const cryptKey= '~!@#*w.(KLH)^F/,W6[jIi]-%kXz+K_w3%+=';
-    
     public function __construct() {
         parent::__construct();
-        $this->db = Factory::getDBO('local_jiada');
         $this->ssodb = Factory::getDBO('local_jiada_sso');
     }
     
     /**
-     * 根据用户名密码获取商家信息
-     * @param array $data
+     * 检测邮箱是否唯一
+     * @param boole or int
      */
-    public function getBusinessInfo($data){
-        $query = "select a.businessId,a.userName,a.contact,a.mobile,a.title,a.provinceId,a.cityId,a.districtId,a.address,a.industryId,a.createTime,a.status from business a where a.password = ? and a.userName = ?";
-        $sth = $this->ssodb->prepare($query);
-        if($sth != FALSE){
-            if($sth->execute(array($data['password'], $data['username']))){
-                $rows = $sth->fetch();
-                return $rows;
-            }
-        }
-        return false;
-    }
-    /**
-     * 根据用户名密码获取店铺管理员信息
-     * @param array $data
-     */
-    public function getShopAdminInfo($data){
-        $query = "select a.adminId,a.userName,a.realName,a.agroupId,a.shopId,a.businessId,b.industryId,a.createTime,a.status,c.acl from business_shop_admin a,business b,business_admin_group c where a.password = ? and a.userName = ? and a.businessId=b.businessId and a.agroupId=c.agroupId";
-        $sth = $this->ssodb->prepare($query);
-        if($sth != FALSE){
-            if($sth->execute(array($data['password'], $data['username']))){
-                $rows = $sth->fetch();
-                return $rows;
-            }
-        }
-        return false;
-    }
-    
-    /**
-     * 根据行业id获取行业信息；
-     * @param array $data
-     */
-    public function getIndustryInfo($industryId){
-        $query = "select a.industryName,a.pinyin,a.public from industry a where a.industryId = $industryId";
-        $this->db->setQuery($query);
-        $rows = $this->db->loadAssoc();
-        return $rows;
-    }
-    
-    /*
-     * 构建客户端唯一ID,并进行加密;
-     * @param int $userId 管理员id
-     * @param string $industryPinyin 行业拼音
-     */
-    public function setTicket($ticketParam) {
-        $ticket = $this->buildTicket($ticketParam);
-        if(!$ticket){
+    public function checkEmail($email){
+        if(!trim($email)){
             return false;
         }
-        $cryptKey = strrev(md5(self::cryptKey));
-        $ticket = strrev(sha1($ticket)).$cryptKey;
-        $ticket = sha1($ticket);
-        return $ticket;
+        $query = "select count(a.userId) from user a where a.email = '$email' limit 0,1";
+        $this->ssodb->setQuery($query);
+        $num = $this->ssodb->loadResult();
+        return $num;
     }
     
     /**
-     * 对字符串进行前面,和密码加密的方式一样
-     * @param type $string
+     * 检测用户名是否唯一
+     * @param boole or int
      */
-    public function getSign($string){
-        $cryptKey = strrev(md5(self::cryptKey));
-        $string = strrev(sha1($string)).$cryptKey;
-        $strSign = sha1($string);
-        return $strSign;
-    }
-
-    /**
-     * 构建原始票据结构:商家id|店铺id|用户id|acl权限|浏览器代理信息|用户ip地址|行业拼音|用户socket端口号;
-     * 如果是商家那么店铺id为空，用户id等于商家id，acl权限也为空；商家是主账号拥有所有权限。
-     * @param int $userId 管理员id
-     * @param string $industryPinyin 行业拼音
-     */
-    public function buildTicket($ticketParam) {
-        if (!$ticketParam['businessId'] || !$ticketParam['industry_modules'])
+    public function checkUserName($userName){
+        if(!trim($userName)){
             return false;
-
-        $IP = Util::getIP();
-        //由于端口号隔一段时间就会变动所以不能使用
-//        $ticket = $userId.'|'.$_SERVER['HTTP_USER_AGENT'].'|'.$IP.'|' . $industryPinyin.'|' . $_SERVER['REMOTE_PORT'];
-        $ticket = $ticketParam['businessId'].'|'.$ticketParam['shopId'].'|'.$ticketParam['uid'].'|'.$ticketParam['acl'].'|'.$_SERVER['HTTP_USER_AGENT'].'|'.$IP.'|' . $ticketParam['industry_modules'];
-        return $ticket;
+        }
+        $query = "select count(a.userId) from user a where a.userName = '$userName' limit 0,1";
+        $this->ssodb->setQuery($query);
+        $num = $this->ssodb->loadResult();
+        return $num;
     }
+    
+    public function uuid_short(){
+       $sql = "select uuid_short();";
+       $this->ssodb->setQuery($sql);
+       $uuid_short = $this->ssodb->loadResult();
+       return $uuid_short;
+    }
+    
+    //用户注册
+    public function reg($data) {
+        $time=time();
+        $userId = $this->uuid_short();
+        if (isset($data['passwd']) && $data['passwd']) {
+            $password=strrev(sha1($data['passwd']));
+            $password=md5($password);
+        } else {
+            return false;
+        }
+        $sql = "insert user set 
+                                    `userId`='$userId',
+                                    `areaId`='$data[community]',
+                                    `userName`='$data[nickname]',
+                                    `password`='$password',
+                                    `email`='$data[email]',
+                                    `mobile`='',
+                                    `emailValidate`='0',
+                                    `mobileValidate`='0',
+                                    `status`='1',
+                                    `registerTime`='$time'";
+        $result = $this->ssodb->query($sql);
+        if ($result == false) {
+            $error = $db->ErrorMsg();
+            die("$error");
+        }
+        return true;
+    }
+    
 
     /**
      * form 验证规则
@@ -201,10 +177,10 @@ class RegModel extends BasicModel{
 		  					"name":"clearxss"
 	 					},
 	 					{
-		  					"name":"regex",
-		  					"value":"/^[A-Za-z0-9_\\\\-]{3,20}$/",
-			  				"message":"%s%应为3到20位的字母、数字、字符和下划线"
-	  					}
+ 						"name":"rangelength",
+ 						"value":"[3,16]",
+ 						"message":"%s%长度为3到16位"
+ 						}
 		  			]
 		  		},
                                 {
